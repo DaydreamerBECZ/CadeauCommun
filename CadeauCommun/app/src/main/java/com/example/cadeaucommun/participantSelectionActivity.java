6d4ca588e63ea7fdb6206c7db83d84cf445e7924
Mark <markawad509@gmail.com>
2022-02-24 23:56:34 -0500

    basically finished the participant selection activity.

---
+++ CadeauCommun/app/src/main/java/com/example/cadeaucommun/participantSelectionActivity.java
@@ -0,0 +1,32 @@
+package com.example.cadeaucommun;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.cadeaucommun.BLL.Model.Participant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class participantSelectionActivity extends AppCompatActivity {
+    List<Participant> participants = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_participant_selection);
+
+        ListView listView = findViewById(R.id.allParticipantsListView);
+
+        Participant p = new Participant("Mark");
+        participants.add(p);
+        Participant p1 = new Participant("Zach");
+        participants.add(p1);
+
+        ListAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, participants);
+        listView.setAdapter(adapter);
+    }
+}
